251eb16b3cca6b637ec5249ac27b92387743bc5b
Shemaryahu Zalmanov <shemaryahuz@gmail.com>
2025-08-07 13:37:10 +0300

    feat: intial express aplication

---
+++ app.js
@@ -0,0 +1,12 @@
+// intitial express aplication
+
+import express from "express";
+
+const app = express();
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, (error) => {
+    if (error) console.error(`Error: ${error.message}`);
+    else console.log(`Server is running on: http://localhost:${port}`);
+})
